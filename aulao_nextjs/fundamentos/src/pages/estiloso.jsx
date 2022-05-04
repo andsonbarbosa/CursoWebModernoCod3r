430cb5373c705f533be3b3446b6f0f84f914a8b9
Andson <andsonsilva2015@hotmail.com>
2022-05-04 18:06:37 -0300

    Adicionando cabeçalho e estilos

--- aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,10 +1,15 @@
 import styles from '../styles/Estiloso.module.css'
-
+import Layout from '../components/layout'
 
 export default function Estiloso(){
     return (
-        <div className={styles.roxo}>
-            <h1>Estilo usando CSS Módulos</h1>
-        </div>
+
+        <Layout titulo="Exemplo de CSS Modularizado">
+            <div className={styles.roxo}>
+                <h1>Estilo usando CSS Módulos</h1>
+            </div>
+        </Layout>
+
+        
     )
 }
