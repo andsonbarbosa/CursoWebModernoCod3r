ae6f6cd63a8cf4abd22f08e340f20e0c343cf6ab
Andson <andsonsilva2015@hotmail.com>
2022-05-08 18:11:55 -0300

    Projeto Cadastro de Usuário(CRUD) utilizando Next.js: Formulario, Tabela e Botões finaliados e fucionando. faltando só o BD.

--- aulao_nextjs/next-crud/src/pages/index.tsx
+++ aulao_nextjs/next-crud/src/pages/index.tsx
@@ -1,7 +1,35 @@
-import Layout from "../components/layout";
+
+import { useState } from "react";
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 24, '2'),
+    new Cliente('Carlos', 37, '3'),
+    new Cliente('Pedro', 46, '4')
+  ]
+
+  function clienteSelecionado(cliente: Cliente){
+
+  }
+
+  function clienteExcluido(cliente: Cliente){
+
+  }
+
+  function salvarCliente(cliente: Cliente){
+    console.log(cliente)
+  }
+
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
   return (
     <div className={`
       flex h-screen justify-center items-center
@@ -9,7 +37,28 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        {visivel === 'tabela' ? (
+        <>
+          <div className="flex justify-end">
+          <Botao className="mb-4" onClick={() => setVisivel('form')}>
+            Novo Cliente
+          </Botao>
+          </div>
+          
+          <Tabela clientes={clientes}
+                  clienteSelecionado={clienteSelecionado} 
+                  clienteExcluido={clienteExcluido} 
+          />
+        </>
+
+        ) : (
+          <Formulario 
+            cliente={clientes[0]}
+            clienteMudou={salvarCliente}
+            cancelado={() => setVisivel('tabela')}
+          />
+        )}
+
       </Layout>
     </div>
   )
