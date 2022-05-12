dd63c07da5e4ca2adb79052a366b0339cda2757f
Andson <andsonsilva2015@hotmail.com>
2022-05-12 16:41:56 -0300

    Projeto: Calculator VueJs

---
+++ vue/calculator/src/components/Display.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="display">
+        {{ value }}
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .display {
+        grid-column: span 4;
+        background-color: #0004;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 20px;
+        font-size: 2.1em;
+        overflow: hidden;
+    }
+</style>
