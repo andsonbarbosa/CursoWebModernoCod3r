ba18a8683a3b74ea4498ebdb81de9f1495483ff9
Andson <andsonsilva2015@hotmail.com>
2022-05-20 11:27:13 -0300

    Angular CRUD: Finalizdo.

--- angular-crud/crud/frontend/src/app/components/product/product-read/product-read.component.ts
+++ angular-crud/crud/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,6 +1,6 @@
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../product.model';
-import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-read',
@@ -9,14 +9,14 @@ import { ProductService } from '../product.service';
 })
 export class ProductReadComponent implements OnInit {
 
-  products: Product[] | undefined;
-
+  products: Product[]
+  displayedColumns = ['id', 'name', 'price', 'action']
+  
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
       this.products = products
-      console.log(products)
     })
   }
 
